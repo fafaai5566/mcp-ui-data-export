49049462e8862e698b1f86a883711e2d06f8593a
fafaai5566 <fafaai2023@gmail.com>
2026-01-08 10:01:43 +0100

    Connect to Analytics

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { StartScreen } from "@/components/shell/StartScreen"
 import { AppShell } from "@/components/shell/AppShell"
 import { useMcpStore } from "@/lib/store/useMcpStore"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Page() {
   const activeWidget = useMcpStore((s) => s.activeWidget)
